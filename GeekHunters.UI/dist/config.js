33f212798cd756f54bbe7ee7a5a31e08bd533173
Kai Sun <kaisun1992@gmail.com>
2018-10-28 23:38:16 +1300

    Removed js files under dist folder and bundle.js

--- GeekHunters.UI/dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    apiBaseUrl: "https://localhost:44370/api"
-};
-//# sourceMappingURL=config.js.map
